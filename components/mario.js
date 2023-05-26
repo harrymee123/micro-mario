3338ca99c1db48dcc28e4cab65b82a612880d268
Harry Mee <harrymee94@gmail.com>
2023-05-26 12:11:51 +0100

    remove nexus

--- components/mario.js
+++ components/mario.js
@@ -12,11 +12,8 @@ const Mario = () => {
       <h1 className={styles.title}>G'day! I'ma Mario.</h1>
       <span>
         I'm hosted at
-        <a
-          target="_blank"
-          href="https://micro-mario-46q24z3yb-harrymee94-gmailcom.vercel.app/mario"
-        >
-          https://micro-mario-46q24z3yb-harrymee94-gmailcom.vercel.app/mario
+        <a target="_blank" href="https://micro-mario.vercel.app/mario">
+          https://micro-mario.vercel.app/mario
         </a>
       </span>
     </main>
